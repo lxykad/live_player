f854775b5b4b6958cd6c3cf8f666fc0c87be494e
1 <a@1deiMac.local>
2017-09-12 13:57:13 +0800

    add url

--- app/src/main/java/com/ijk/live/MainActivity.java
+++ app/src/main/java/com/ijk/live/MainActivity.java
@@ -1,17 +1,23 @@
 package com.ijk.live;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.ijk.live.application.Settings;
 import com.ijk.live.player.IjkVideoView;
 
+import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
     private String url = "http://9890.vod.myqcloud.com/9890_9c1fa3e2aea011e59fc841df10c92278.f20.mp4";
+    private String url2 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
     private IjkVideoView mVideoView;
+
+    private Settings mSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mVideoView = (IjkVideoView) findViewById(R.id.video_view);
+        mSetting= new Settings(this);
 
         IjkMediaPlayer.loadLibrariesOnce(null);
         IjkMediaPlayer.native_profileBegin("libijkplayer.so");
-        mVideoView.setVideoURI(Uri.parse(url));
-        mVideoView.start();
+
+
+//        mVideoView.setVideoURI(Uri.parse(url));
+//        mVideoView.start();
+
+        mVideoView.setVideoPath(url);
+
+        mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(IMediaPlayer iMediaPlayer) {
+                mVideoView.start();
+
+
+            }
+        });
+
+        mVideoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"click",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoView.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
